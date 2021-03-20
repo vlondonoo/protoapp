456d5c067bef28f6995e21505edb3b18f2cf863c
vlondonoo <vlondonoo@gmail.com>
2021-03-19 19:52:45 -0500

    first

---
+++ src/main/java/com/mycompany/protoapp/Calculadora.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.protoapp;
+
+/**
+ *
+ * @author vlond
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+   public Double cuadrado(Double numero) {
+        return numero * numero;
+    }
+}
